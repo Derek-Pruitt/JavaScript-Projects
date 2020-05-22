5b6d81fd0702f19a677795afe40918ba50fc7ef9
Derekhunterrc0001 <64759619+Derekhunterrc0001@users.noreply.github.com>
2020-05-22 15:53:24 -0600

    Projects

---
+++ Basic JavaScript Projects/JS/Basic_Javasrcipt_1.js
@@ -0,0 +1,32 @@
+window.alert('Hello, world!');// making a pop up to display the string
+
+document.write('Hello, world!<br>');//displaying string
+
+var A=  "My name is Derek<br>"//assigning a variable
+document.write(A);//displaying string
+
+var B=" I really enjoy doing this!";//assigning a variable
+window.alert(B);//making variable B a pop up
+
+document.write("\"My dream is to make a better life for my wife and I, wouldent you agree?\"<br>\'Yes I would.\'<br>");//displaying string
+
+document.write("\"I have learned HTML and CSS. "
++"And now I am learning Java\Script.\"<br>");//displaying string
+
+var C="I hope to land a job soon. "//assigning a variable
+
+var D="Because I would really like to quit my "//assigning a variable thats a concatenate
++"current job.<br>"
+document.write(C,D);// concatenate the variables C and D together
+
+
+var DreamJob="microsoft", job2="Infintiy ward", job3="Google"//assigning a multi variable
+document.write(job2);//displaying variable
+var DreamJob=DreamJob.fontcolor("green")//assigning a variable a color
+var job2=job2.fontcolor("blue")//assigning a variable a color
+var job3=job3.fontcolor("red")//assigning a variable a color
+document.write(job3);//displaying variable in color
+document.write(job2);//displaying variable in color
+document.write(DreamJob);//displaying variable in color
+
+var me="Derek Pruitt"//writing a expression
--- JS/Basic_Javasrcipt_1.js
+++
@@ -1,4 +0,0 @@
-window.alert('Hello, world!');
-document.write('Hello, world!');
-var A=  "My name is Derek";
-document.write(A);
