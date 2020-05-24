efc58b4d9fcb02934da9dd2099f03f7bad56f984
Derekhunterrc0001 <64759619+Derekhunterrc0001@users.noreply.github.com>
2020-05-24 15:51:00 -0600

    class work

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,61 @@
+document.write(typeof"word");
+document.write(typeof 3);
+function Testing() {//isNaN("")'not a number'
+    document.getElementById("False").innerHTML=isNaN('007');
+}
+function TE() {
+    document.getElementById("NaN").innerHTML=0/0;
+}
+function Te() {//isNaN("")'not a number'
+    document.getElementById("True").innerHTML=isNaN('This is not a number');
+}
+
+document.write(2E310);//infinity
+
+document.write(-3E310);//-infinity
+
+function Inf() {//displaying a positive infinity
+    var K=92.26734736472364264893246E310
+    document.getElementById("Infinity").innerHTML=K;
+}
+
+function NInf() {//displaying a negitive infinity
+    var J=-63.9834723746E345
+    document.getElementById("NInfinity").innerHTML=J;
+}
+document.write(10>2);//boolean logic-true or false
+document.write(10<2);
+
+console.log(2+2);//displays in the developer tools under console
+
+document.write("thirteen"+20);//type coercion
+
+console.log(20<2[document.write]);//showing false in the console
+
+document.write(10+20==30);//checking for equality
+document.write(10+20==20);
+
+//=== used to instruct to check if data on right equal to left and the same type of data as that on the right(true or false)
+x=10;
+y=10;
+document.write(x===y);
+o=10;
+p="20";
+document.write(o===p);
+Mi="10";
+Mo=10;
+document.write(Mi===Mo);
+Ja=10;
+Js=11;
+document.write(Ja===Js);
+//logical operators AND-&&/OR-||/NOT-!
+document.write(10>5&&10>4);
+document.write(10<2&&10>9);
+document.write(4>10||10>9);
+document.write(19>20||10>11);
+function not_function() {
+    document.getElementById("Not").innerHTML=!(5>10);
+}
+function not1_function() {
+    document.getElementById("Not1").innerHTML=!(10>9);
+}
