46c14b17bb38d20a4deee761b45cd2ce2c2fe059
Derekhunterrc0001 <64759619+Derekhunterrc0001@users.noreply.github.com>
2020-05-23 19:32:47 -0600

    class work

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_4.js
@@ -0,0 +1,23 @@
+function Dic() {
+    var car= {
+        make:"ford",
+        color:"red",
+        doors:"two door",
+        engein:"v8",
+        passangers:4,
+        sound:"vroooom!"
+    };
+    document.getElementById("dictionary").innerHTML = car.sound;
+}
+function DOOM() {
+    var BooM= {
+        make:"chevy",
+        doors:"blue",
+        doors:"four door",
+        engein:"v6",
+        passangers:4,
+        sound:"peter pater!"
+    };
+    delete BooM.sound;
+    document.getElementById("ictionary").innerHTML = BooM.sound;
+}
